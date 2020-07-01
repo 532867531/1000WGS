1931ee0304ec0bba9090ca27ace08669cf8c7b18
532867531@qq.com <532867531@qq.com>
2020-07-01 11:20:14 +0800

    添加了注释 Annotations added

--- 00_intervel_according2fasta.dict.r
+++ 00_intervel_according2fasta.dict.r
@@ -1,28 +1,35 @@
 setwd("D:/华大/1000WGS/300GATK")
 list.files()
+
+##读取参考基因组字典文件dict
 dict=read.delim(file = "C:/Users/Administrator/Documents/Homo_sapiens_assembly38.dict",stringsAsFactors = F,header = F)
 dict$V3=as.numeric(sub(pattern = "LN:",replacement = "",x = dict$V3))
 dict$V2=sub(pattern = "SN:",replacement = "",x = dict$V2)
+##选取需要的contig
 dict=dict[c(2:3367),]
 dict=dict[c(2:26),]
 
+##最终存放interval的列表以及interval 大小
 intervals=list()
 length_intervals=60000000
 
+##汇总总共需要分割的bp长度
 sums=sum(dict$V3)
 
-dataframe1=data.frame(CUM=cumsum(dict$V3),CHR=dict$V2,LEN=dict$V3,stringsAsFactors = F)
-dataframe2=data.frame(CUM=c(c(1:(sums/length_intervals))*length_intervals,sums),LEN=NA,CHR=NA,stringsAsFactors = F)
+dataframe1=data.frame(CUM=cumsum(dict$V3),CHR=dict$V2,LEN=dict$V3,stringsAsFactors = F)##实际累积milestone
+dataframe2=data.frame(CUM=c(c(1:(sums/length_intervals))*length_intervals,sums),LEN=NA,CHR=NA,stringsAsFactors = F)##参照累积milestone
 
 dataframe=rbind(dataframe1,dataframe2)
 dataframe=dataframe[order(dataframe$CUM),]
+
+
 minus=0
 for(rowindex in c(1:nrow(dataframe))){
   if(grepl(pattern = "chr",x = dataframe[rowindex,"CHR"])){
-    minus=dataframe[rowindex,"CUM"]
+    minus=dataframe[rowindex,"CUM"]                           ##用于更换contig时坐标相对置0
     dataframe[rowindex,"CUTOFF"]=dataframe[rowindex,"LEN"]
   }else{
-    dataframe[rowindex,"CUTOFF"]=dataframe[rowindex,"CUM"]-minus
+    dataframe[rowindex,"CUTOFF"]=dataframe[rowindex,"CUM"]-minus  ##用于更换contig时坐标相对置0
   }
 }
 ##倒序标染色体
@@ -36,12 +43,15 @@ for(rowindex in c(nrow(dataframe):1)){
 }
 dataframe=dataframe[which(!is.na(dataframe$CHR)),]
 
+##分contig聚合milestone节点
 data=aggregate(x = dataframe,by = list(CHR=dataframe$CHR),FUN = function(x){paste(x,collapse = "|")})
 rownames(data)=data$CHR
 data=data[unique(dataframe$CHR),]
 
 intervals=c()
 intervals_dataframe=na.omit(data.frame(CHR=NA,BEGIN=NA,END=NA,stringsAsFactors = F))
+
+
 ##按照每个染色体interval
 for(rowindex in c(1:nrow(data))){
   CHR=data[rowindex,"CHR"]
